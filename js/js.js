dd88d4a818d4611d364bfe89c7f7d1dd83c1e80f
Frederikke <rfrederikke@gmail.com>
2020-12-04 10:08:46 +0100

    Om Café Parasollen
    Kode til underside om caféen

---
+++ js/js.js
@@ -0,0 +1,19 @@
+/*
+ * fil: js.js
+ * purpose: introdction to jQuery
+ */
+
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+$(document).ready(function () { // kører så snart DOM er klar
+
+    console.log("jQuery 3.5.1 running. Alert level green.");
+
+
+    }).catch(err => {
+
+        console.log('There was an error ...');
+    });
+
+}); // denne line må ikke slettes
