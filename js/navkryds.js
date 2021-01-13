7189e6f107c5122af0371817a8a6f82bc67a7e9e
Mae-Berces <eaajmmb@students.eaaa.dk>
2021-01-13 21:53:03 +0100

    kommentar

--- js/navkryds.js
+++ js/navkryds.js
@@ -2,7 +2,7 @@
  $(document).ready(function () { // kører så snart DOM er klar
 
      console.log("jQuery 3.5.1 running. Alert level green.");
-     $("#kryds").click(function () {
+     $("#kryds").click(function () {   // Denne fortæller browseren at hente/udføre et script når et element klikkes med musen.
          $('.nav-list').removeClass('open');
          console.log("klik");
      })
