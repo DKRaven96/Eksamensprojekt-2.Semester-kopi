e107c1a43445625d24debe91476196c406c21f2d
diana <dianamyjak@hotmail.dk>
2021-01-14 20:42:41 +0100

    javascript kommentar

--- js/js.js
+++ js/js.js
@@ -3,13 +3,9 @@
  * purpose: introdction to jQuery
  */
 
-<<<<<<< Updated upstream
-/** JS er vigtig for UX da den gør brugeren mere engageret samt opfordre til interaktion**/
+/** JS er vigtigt for UX da den brugeren mere engageret samt opfodre til interaktion**/ 
 
-console.log('file: js/js.js loaded');
-=======
 console.log('file: js/js.js loaded'); //console.log er en funktion der bruges til at udskrive en hver form for variabler(img,audio osv), der er defineret før i den eller til bare at udskrive enhver meddelelse , der skal vises til brugeren.
->>>>>>> Stashed changes
 
 // A $( document ).ready() block.
 $(document).ready(function () { // kører så snart DOM er klar
@@ -17,21 +13,29 @@ $(document).ready(function () { // kører så snart DOM er klar
     console.log("jQuery 3.5.1 running. Alert level green.");
 
 
-// nede stående funktion er taget fra Webdok, men bruges ikke til noget. 
+// nede stående funktion er taget fra Webdok( scroll funktion i forhold til navbar), men bruges ikke til noget. Derfor kan den også slettes. 
+
 });
 
-
+$
+    (window).on("scroll", function () {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('black');
+        } else {
+            $('nav').removeClass('black');
+        }
+    })
 
 /**
- * Audio controls (again, find method to do it in ONE function rather than 2)
+ * Audio controls (gør det muligt at gøre det i en funktion i stedet for  2)
  **/
 var $audioIconPhilip = $("#audioIconPhilip");
-var audioPhilip = $("#audioPhilip").get(0); /* get(0) henter den følgende funktion i JavaScript, frem for i JQuery */
+var audioPhilip = $("#audioPhilip").get(0); /* get(0) henter den følgende funktion i JavaScript, frem for i JQuery, det gør vi for at hente det specifikke element, */
 
 $audioIconPhilip.on('click', function () {
-    if (audioPhilip.paused) {     //Brug af if til at angive en blok, der skal udføres hvis en specificeret betingelse er sand
+    if (audioPhilip.paused) {     //Brug af if til at angive en blok, der skal udføres hvis en specificeret betingelse er sand (vi bruger den til at starte audio)
         audioPhilip.play();
-    } else {                      //Brug af else til at specificere en blok kode der skal udføres hvis den sammen betingelse er falsk
+    } else {                      //Brug af else til at specificere en blok kode der skal udføres hvis den sammen betingelse er falsk, vi bruger den til at pause Audio
         audioPhilip.pause();
     }
 });
@@ -48,12 +52,12 @@ $audioIconVeyis.on('click', function () {
 });
 
 // slideshow menu
-var slideIndex = 1;
+var slideIndex = 1; // Var= variable, value 1 = første billede
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(n) { // n = nuværende billede
+    showSlides(slideIndex += n); // tilføjer en værdi til n
 }
 
 // Thumbnail image controls
@@ -66,17 +70,17 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1 // vis første billede
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length // vis næste billede i rækken
     }
-    for (i = 0; i < slides.length; i++) {
+    for (i = 0; i < slides.length; i++) { // laver en endeløs loop gennem det fulde antal af billeder, start forfra når det er nået
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (i = 0; i < dots.length; i++) { // laver en endeløs loop gennem det fulde antal af prikker, start forfra
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block"; // skjuler andre billeder som ikke er billede 1
+    dots[slideIndex - 1].className += " active"; // klikker man videre fra 1. billede bliver en ny dot aktiv
 }
